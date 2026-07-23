1fbf78a7bbffad81efe8028735b4980b089c413a
Robin <robinrukhaya54@gmail.com>
2026-07-23 17:07:47 +0530

    Time: 0 ms (100%), Space: 16.7 MB (70.38%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int  n  = grid.size();
+        int n = grid.size();
         int m = grid[0].size();
-
-        queue<pair<int,int>> q;
+        int ans = 0;
         int fresh = 0;
 
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j <m;j++){
-                if(grid[i][j] == 2)
-                    q.push({i,j});
-                else if(grid[i][j]==1)
-                   fresh++;
-            }
-        }
-
-        int time = 0;
-        int dr[4] = {1,-1,0,0};
-        int dc[4] = {0,0,1,-1};
-
-        while(!q.empty() && fresh >0) {
-            int size = q.size();
-
-            while(size--) {
-                auto[r,c] = q.front();
-                q.pop();
-
-                for(int k = 0;k<4;k++){
-                    int nr= r+dr[k];
-                    int nc = c+dc[k];
-
-                    if(nr >= 0 && nr <n  && nc >= 0 && nc <m &&
-                     grid[nr][nc]==1) {
-                      grid[nr][nc]= 2;
-                      fresh--;
-                      q.push({nr,nc});
-                    }
+        queue<pair<int, pair<int,int>>> q;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m;j++){
+                if(grid[i][j] == 2){
+                    q.push({0,{i,j}});
+                }
+                if(grid[i][j] == 1){
+                    fresh++;
+                   
                 }
             }
-            time++;
         }
-        return fresh ==0 ? time: -1;
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+
+            int time = node.first;
+            int row =  node.second.first;
+            int col =  node.second.second;
+
+            int dr[] = {-1,1,0,0};
+            int dc[] = {0,0,-1,1};
+            for(int i = 0;i <4;i++){
+                int newRow = row+dr[i];
+                int newCol = col+dc[i];
+
+                if(newRow >=0  && newRow <n && newCol >= 0 && newCol <m && grid[newRow][newCol] == 1) {
+                    grid[newRow][newCol] = 2;
+                    fresh--;
+                    q.push({time+1,{newRow,newCol}});
+                }
+            }
+            ans= max(ans,time);
+            
+        }
+        
+        if(fresh >0){
+            return -1;
+        }
+        return ans;
     }
 };

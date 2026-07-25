c9e20d1093d8f65d53e2d077edfd68ab2abc7d26
Robin <robinrukhaya54@gmail.com>
2026-07-25 18:50:57 +0530

    Time: 27 ms (27.15%), Space: 34.8 MB (66.89%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,42 +2,40 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
-        if(n == 0) return {};
+        int m = mat[0].size();
 
-        int  m = mat[0].size();
-
-        queue<pair<int,int>> q;
-        vector<vector<int>> dist(n,vector<int>(m,0));
-        vector<vector<int>> vis(n,vector<int>(m,0));
-
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                if(mat[i][j]== 0) {
+        queue<pair<int,int>>q;
+        
+        vector<vector<int>> dist(n,vector<int>(m,-1));
+        for(int i  =0; i <n;i++){
+            for(int j= 0;j <m;j++){
+                if(mat[i][j] == 0){
                     q.push({i,j});
-                    vis[i][j]= 1;
-                    dist[i][j]= 0;
+                    dist[i][j] = 0;
                 }
             }
         }
+        while(!q.empty()){
 
-        int dr[4] = {-1,1,0,0};
-        int dc[4] =  {0,0,-1,1};
-
-        while(!q.empty()) {
-            auto[r,c] = q.front();
+            auto node = q.front();
             q.pop();
 
-            for(int k = 0; k<4;k++){
-                int nr = r +dr[k];
-                int nc = c +dc[k];
+            int row = node.first;
+            int col = node.second;
 
-                if(nr >= 0 && nr <n && nc >= 0 && nc <m && !vis[nr][nc]) {
-                    vis[nr][nc]= 1;
-                    dist[nr][nc] = dist[r][c]+1;
-                    q.push({nr,nc});
+            int dr[] = {-1,1,0,0};
+            int dc[] = {0,0,-1,1};
+
+            for(int i =0; i < 4;i++){
+                int newRow = row+dr[i];
+                int newCol = col+dc[i];
+            
+                if(newRow >=0 && newRow < n && newCol >=0 && newCol < m &&dist[newRow][newCol] == -1){
+                    dist[newRow][newCol] = dist[row][col]+1;
+                    q.push({newRow,newCol});
                 }
             }
         }
         return dist;
     }
-}; 
+};
